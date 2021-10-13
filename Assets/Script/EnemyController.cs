6cc8c7a731df51a25f4bce94b72e8c5c4e75443d
MutoAyumu <mutoayum0610@gmail.com>
2021-10-13 14:28:31 +0900

    敵のスクリプトの追加・HPプレハブの追加

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
+public class EnemyController : MonoBehaviour
+{
+    [Header("HP")]
+    [SerializeField, Tooltip("HPの値")] float m_hp = 3f;
+    float m_currentHp;
+    [SerializeField, Tooltip("HPを表示するスライダー")] Slider m_hpSlider = default;
+    [Space(10)]
+    [Header("動き")]
+    [SerializeField] float m_moveSpeed = 5f;
+
+    private void Start()
+    {
+        m_currentHp = m_hp;
+        m_hpSlider.value = 1;
+    }
+    private void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        m_hpSlider.transform.rotation = Camera.main.transform.rotation;
+    }
+    public void TakeDamage(float damage)
+    {
+        if (m_currentHp >= 0)
+        {
+            m_currentHp -= damage;
+            m_hpSlider.value = m_currentHp / 1;
+        }
+    }
+}
--- Assets/Script/PlayerMoveController.cs
+++ Assets/Script/PlayerMoveController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMoveController : MonoBehaviour
 {
     Rigidbody m_rb;
     Animator m_anim;
     Vector3 m_move;
+    EnemyController m_enemy;
 
     Quaternion m_rotation;
     float m_h;
@@ -20,6 +23,8 @@ public class PlayerMoveController : MonoBehaviour
     [SerializeField, Tooltip("ジャンプの数値")] float m_jumpPower = 5f;
     [Space(10), Header("各種設定")]
     [SerializeField, Tooltip("回転の滑らかさ")]float rotationSpeed = 7f;
+    [SerializeField, Tooltip("攻撃力")] float m_attackPower = 1f;
+    [SerializeField, Tooltip("ダメージを与える敵のタグ")] string m_enemyTag = "";
 
     private void Start()
     {
@@ -39,7 +44,7 @@ public class PlayerMoveController : MonoBehaviour
         m_v = Input.GetAxis("Vertical");
         m_move = new Vector3(m_h, 0, m_v).normalized;
 
-        if(Input.GetButton("Shift"))
+        if(Input.GetButton("Shift") && (m_h != 0 || m_v != 0))
         {
             //今のスピード値とパラメータを変更
             m_currentSpeed = m_dushSpeed;
@@ -62,10 +67,18 @@ public class PlayerMoveController : MonoBehaviour
             Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
         }
+
+        //攻撃のアニメーションを流す
+        if(Input.GetButtonDown("Fire1"))
+        {
+
+        }
     }
     void UpdateMove()
     {
+        //動きのベクトルをCameraのローカル座標系に変更
         m_move = Camera.main.transform.TransformDirection(m_move);
+        //Ｙ成分だけを0にする
         m_move.y = 0;
 
         //移動方向を向く
@@ -81,5 +94,31 @@ public class PlayerMoveController : MonoBehaviour
         //パラメータにXYの値を入れる
         m_anim.SetFloat("X", m_h);
         m_anim.SetFloat("Y", m_v);
+    }
+    void InputAttack()
+    {
+        if(m_enemy != null)
+        {
+            m_enemy.TakeDamage(m_attackPower);
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //攻撃対象の取得
+        if(gameObject.CompareTag(m_enemyTag))
+        {
+            m_enemy = other.gameObject.GetComponent<EnemyController>();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        //攻撃対象の情報破棄
+        if(gameObject.CompareTag(m_enemyTag))
+        {
+            m_enemy = default;
+        }
     }
 }
